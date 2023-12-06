04d22b0d08da488152e8f82811fda52d9fb0ebea
Alessandro Coelho Porto <acporto@stj.jus.br>
2023-12-06 13:46:40 -0300

    Adicionando DICA para uso do token.

--- javaGCS/src/Main.java
+++ javaGCS/src/Main.java
@@ -1,5 +1,6 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+// TOKEN GIT HUB: https://<token sem maior e menor>@github.com/acporto16/ProjetoGCS
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
@@ -16,7 +17,5 @@ public class Main {
         cliente1.retirar(3000);
 
         System.out.println(cliente1);
-
-
     }
 }
